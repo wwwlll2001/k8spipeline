50610092dad5f7b5ed8b5a3897dfab743090171b
wwwlll2001 <wwwlllcat2003@126.com>
2020-06-04 09:07:51 +0800

    Initial commit

---
+++ src/main/java/demo/k8spipeline/K8spipelineApplication.java
@@ -0,0 +1,13 @@
+package demo.k8spipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K8spipelineApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(K8spipelineApplication.class, args);
+    }
+
+}
---
+++ src/test/java/demo/k8spipeline/K8spipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package demo.k8spipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class K8spipelineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
