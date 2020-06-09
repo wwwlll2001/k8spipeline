36dd6411a0e4604386e8d8fb50120d15555b2d66
wwwlll2001 <wwwlllcat2003@126.com>
2020-06-09 10:35:04 +0800

    [wanglei] feat: add configuration for greeting

--- src/main/java/demo/k8spipeline/k8spipelineController.java
+++ src/main/java/demo/k8spipeline/k8spipelineController.java
@@ -1,6 +1,7 @@
 package demo.k8spipeline;
 
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,6 +9,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 //@RequestMapping("/apple")
 public class k8spipelineController {
+
+    @Value("${greeting}")
+    private  String greeting;
 
     @GetMapping("/how")
     public String howAreU() {
